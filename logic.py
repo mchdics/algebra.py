09225d8f36f808187f48a9aeef0dbf4f4e2d5dd5
mehdi <mehdi@SYSTEM.lan>
2025-09-01 17:33:58 -0400

    Fixed bugs

--- logic.py
+++ logic.py
@@ -2,47 +2,57 @@ def inputEq():
     validator = 0
     while validator == 0:
         eq = input("Enter an equation: ")
-        terms = eq.split()
-        t0, t1, t2, t3, t4 = terms
+        parts = eq.split()
+
         #Validation
-        if t3 != "=" and t1 != "=":
-            print("Your equation does not contain an equal sign. Try again: ")
+        if len(parts) != 5:
+            print("Your equation is not composed of exactly 5 parts. Try again: ")
         else:
-            validator = 1
-            return terms
+            p0, p1, p2, p3, p4 = parts
+            if p3 != '=' and p1 != '=':
+                print("Your equation does not contain an equal sign. Try again: ")
+            else:
+                validator = 1
+                return parts
 
-def parseEq(terms):
-    t0, t1, t2, t3, t4 = terms
+def parseEq(parts):
+    p0, p1, p2, p3, p4 = parts
 
-    #t0 t1 t2 = t4
-    if t3 == "=":
-        lhs = (t0, t1, t2)
-        rhs = t4
-        b = int(t4)
-        op = t1
+    #p0 p1 p2 = p4
+    if p3 == '=':
+        lhs = (p0, p1, p2)
+        rhs = p4
+        b = int(p4)
+        op = p1
 
         #Determining a and x position
-        if "a" <= t2 <= "z":
-            varLetter = t2
-            a = int(t0)
-        elif "a" <= t0 <= "z":
-            varLetter = t0
-            a = int(t2)
+        if 'a' <= p2 <= 'z':
+            varLetter = p2
+            a = int(p0)
+        elif 'a' <= p0 <= 'z':
+            varLetter = p0
+            if op == '-':
+                a = -(int(p2))
+            else:
+                a = int(p2)
         else: print("Variable not in correct position.")
 
-    #t0 = t2 t3 t4
-    elif t2 == "=":
-        lhs = t0
-        rhs = (t2, t3, t4)
-        b = int(t0)
-        op = t2
+    #p0 = p2 p3 p4
+    elif p1 == '=':
+        lhs = p0
+        rhs = (p2, p3, p4)
+        b = int(p0)
+        op = p3
 
-        if "a" <= t2 <= "z":
-            varLetter = t2
-            a = int(t4)
-        elif "a" <= t4 <= "z":
-            varLetter = t4
-            a = int(t2)
+        if 'a' <= p4 <= 'z':
+            varLetter = p4
+            a = int(p2)
+        elif 'a' <= p2 <= 'z':
+            varLetter = p2
+            if op == '-':
+                a = -(int(p4))
+            else:
+                a = int(p4)
         else: print("Variable not in correct position.")
     else:
         print("Equal sign not in correct position.")
@@ -50,10 +60,10 @@ def parseEq(terms):
 
 def solveEq(a, b, op):
     varValue = 0
-    if op == "+": varValue = b - a
-    if op == "-": varValue = a - b
-    if op == "*": varValue = b / a
-    if op == "/": varValue = a / b
+    if op == '+': varValue = b - a
+    if op == '-': varValue = a - b
+    if op == '*': varValue = b // a
+    if op == '/': varValue = a * b
     return varValue
 
 def printSolution(varLetter, varValue):
--- main.py
+++ main.py
@@ -1,55 +1,10 @@
+from logic import inputEq, parseEq, solveEq, printSolution
+
 def main():
-    validator = 0
-    while validator == 0:
-        eq = input("Enter an equation: ")
-        parts = eq.split()
-        p0, p1, p2, p3, p4 = parts
-        #Validation
-        if p3 != "=" and p1 != "=":
-            print("Your equation does not contain an equal sign. Try again: ")
-        else: validator = 1
-    
-    #p0 p1 p2 = p4
-    if p3 == "=":
-        lhs = (p0, p1, p2)
-        rhs = p4
-        b = int(p4)
-        op = p1
-
-        #Determining a and x position
-        if "a" <= p2 <= "z":
-            varLetter = p2
-            a = int(p0)
-        elif "a" <= p0 <= "z":
-            varLetter = p0
-            a = int(p2)
-        else: print("Variable not in correct position.")
-    #p0 = p2 p3 p4
-    elif p2 == "=":
-        lhs = p0
-        rhs = (p2, p3, p4)
-        b = int(p0)
-        op = p2
-
-        if "a" <= p2 <= "z":
-            varLetter = p2
-            a = int(p4)
-        elif "a" <= p4 <= "z":
-            varLetter = p4
-            a = int(p2)
-        else: print("Variable not in correct position.")
-    else:
-        print("Equal sign not in correct position.")
-
-    #Logic
-    varValue = 0
-    if op == "+": varValue = b - a
-    if op == "-": varValue = a - b
-    if op == "*": varValue = b / a
-    if op == "/": varValue = a / b
-
-    #Display
-    print(f"{varLetter} = {varValue}")
+    parts = inputEq()
+    a, b, op, varLetter = parseEq(parts)
+    varValue = solveEq(a, b, op)
+    printSolution(varLetter, varValue)
 
 if __name__ == "__main__":
     main()
